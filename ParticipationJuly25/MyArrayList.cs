bdd764c8d7aa2dea8f75fed9c6ec7b20aa51e018
Oscar Hernandez <oscar_51293@hotmail.com>
2024-08-10 01:52:04 -0700

    update

---
+++ ParticipationJuly25/MyArrayList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace ListsDataStructure
+{
+    namespace ListsDataStructure
+    {
+        public class MyArrayList
+        {
+            int[] _list;
+            int _size = 0;
+            public int Size { get { return _size; } }
+            public int[] List { get { return _list; } }
+
+            public MyArrayList()
+            {
+                _list = new int[10];
+            }
+
+            public MyArrayList(int size)
+            {
+                _list = new int[size];
+            }
+
+            /// <summary>
+            /// Will add the incoming value to the end of the list
+            /// </summary>
+            public void Append(int value)
+            {
+                IncreaseCapacity();
+                _list[_size++] = value;
+            }
+
+            public void AddStart(int value)
+            {
+                IncreaseCapacity();
+                for (int i = _size; i >= 0; i--)
+                {
+                    _list[i + 1] = i;
+                }
+
+                _list[0] = value;
+                ++_size;
+            }
+
+            private void IncreaseCapacity()
+            {
+                if (_size != _list.Length) return;
+                int[] newArray = new int[_size * 2];
+                for (int i = 0; i < _size; i++)
+                {
+                    newArray[i] = _list[i];
+                }
+
+                _list = newArray;
+            }
+        }
+    }
+}
+
---
+++ ParticipationJuly25/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ListsDataStructure
+{
+    namespace ListsDataStructure
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+
+                int[] arr = new int[10];
+
+                List<int> arr1 = arr.Append(2).ToList();
+
+                string[] arrString = { "Hello", "World", "Hi" };
+
+                arr1.Add(2);
+                PrintArray(arr);
+                PrintArray(arrString);
+                PrintArray(arr1.Where(x => x == 43).ToArray());
+            }
+
+            public static void PrintArray<T>(T[] arr)
+            {
+                foreach (T i in arr)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
